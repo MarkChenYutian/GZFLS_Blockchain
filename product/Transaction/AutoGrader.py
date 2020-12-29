1c2985df59e651a98f72edef57c33d60e43c2bd6
MarkChenYutian <markchenyutian@gmail.com>
2020-12-29 08:16:14 +0800

    Update Autograder and add Parent Class BlockchainException

--- product/Transaction/AutoGrader.py
+++ product/Transaction/AutoGrader.py
@@ -3,7 +3,7 @@ from Transaction import *
 from Ledger import *
 from RSA_func import generateMyKeys
 
-print("Autograder Start @ {}\nV 1.0.0 By Mark \nIf you have any problem with Autograder, "
+print("Autograder Start @ {}\nV 1.1.1 By Mark \nIf you have any problem with Autograder, "
       "ask on the WeChat group directly\n--------------------------".format(datetime.datetime.now()))
 print("Setting Test Environment ...", end="", flush=False)
 
@@ -21,13 +21,13 @@ FATAL = "\033[1;31m[Fatal] |"
 WARNING = "\033[1;33m[Warn]  |"
 INFO = "\033[1;34m[Info]  |"
 
-print("\r\033[4;;mTest Case 1. Create Ledger and RSA_keys to setup test environment")
+print("\rTest Case 1. Create Ledger and RSA_keys to setup test environment")
 try:
     test_Ledger = Ledger()
     print(PASS + "\033[0;;m Ledger is created.")
-except:
-    print(FATAL + "\033[0;;m We failed to create a Ledger Object. Autograder Terminated.")
-    raise NotImplementedError()
+except Exception as e:
+    print(FATAL + "\033[0;;m We failed to create a Ledger Object. \nAutograder Terminated.")
+    raise e
 
 names = ["A", "B", "C", "D", "E"]
 try:
@@ -51,7 +51,7 @@ else:
 #####################################################
 
 ############# Transaction from COINBASE ##############
-print("\n\033[4;;mTest Case 2. Creating Simple Transactions between Users")
+print("Test Case 2. Creating Simple Transactions between Users")
 try:
     for name in names:
         newTransaction = Transaction(test_Ledger, 50, (0, 0), (0, 0), RSA_keys[name][1], isCoinBase=True)
@@ -61,7 +61,7 @@ try:
         WARNING_Count += 1
         print(
             WARNING + "\033[0;;m Some (All) of the Transactions from COINBASE failed to add into the ledger. "
-                      "This may be caused by improper validation in Ledger.addTransaction. \nAutograder will "
+                      "This may cause by improper validation in Ledger.addTransaction. \nAutograder will "
                       "continue to run.")
     else:
         print(PASS + "\033[0;;m Transaction from COINBASE is accepted.")
@@ -105,7 +105,7 @@ except Exception as e:
 
 ########## Malicious Transaction Test ################
 
-print("\n\033[4;;mTest Case 3. Malicious Transaction Test")
+print("Test Case 3. Malicious Transaction Test")
 
 # CASE 1 | A --- [10 x] 20 ---> B
 
@@ -123,6 +123,9 @@ try:
                     "Autograder will continue to run.")
 except TranactionNotBalanceError:
     print("\r" + PASS + "\033[0;;m Ledger detect the modified Transaction and raise TransactionNotBalanceError as expected.")
+except BlockchainError as e:
+    WARNING_Count += 1
+    print("\r" + WARNING + "\033[0;;m Ledger detect the modified Transaction but raise {} instead of TransactionNotBalanceError.".format(type(e)))
 except Exception as e:
     print("\r" + FATAL + "\033[0;;m When we are trying to add a malicious modified transaction into the ledger, "
                   "Unexpected Exception is raised. Check whether your program raise the "
@@ -143,6 +146,9 @@ try:
                             "Autograder will continue to run.")
 except TransactionSignatureError:
     print("\r" + PASS + "\033[0;;m Ledger detect the signature created by A is invalid and raise TransactionSignatureError as expected.")
+except BlockchainError as e:
+    WARNING_Count += 1
+    print("\r" + WARNING + "\033[0;;m Ledger detect the modified Transaction but raise {} instead of TransactionSignatureError.".format(type(e)))
 except Exception as e:
     print("\r" + FATAL + "\033[0;;m When we are trying to add a malicious modified transaction into the ledger, "
                   "Unexpected Exception is raised. Check whether your program raise the "
@@ -165,6 +171,9 @@ try:
 except TransactionInNotExist:
     print("\r" + PASS + "\033[0;;m Ledger detect that the input transaction of the new transaction is invalid"
                         " and raise TransactionInNotExist as expected.")
+except BlockchainError as e:
+    WARNING_Count += 1
+    print("\r" + WARNING + "\033[0;;m Ledger detect the modified Transaction but raise {} instead of TransactionInNotExist.".format(type(e)))
 except Exception as e:
     print("\r" + FATAL + "\033[0;;m When we are trying to add a malicious modified transaction into the ledger, "
                   "Unexpected Exception is raised. Check whether your program raise the "
@@ -173,11 +182,31 @@ except Exception as e:
                   "Detailed Exception is raised.")
     raise e
 
+# CASE 4 | Check whether the attempt to add invalid transactions into ledger will lead to change on balance
+
+try:
+    assert len(set(test_Ledger.getBalanceStat().values()) - {40, 60, 30, 70, 50}) == 0
+    print(PASS + "\033[0;;m Attempt to add invalid transactions does NOT change the balance in ledger.")
+except AssertionError as e:
+    WARNING_Count += 1
+    print(WARNING + "\033[0;;m Adding invalid transactions into the ledger cause the ledger balance change.\n"\
+                    "Probably because - 1. The invalid transaction in previous test case is accepted by the ledger\n"\
+                    "2. The sequence to modify the 'isUsed' state in ledger and validate input transaction is incorrect.\n"\
+                    "Autograder will continue to run.")
+except Exception as e:
+    print(FATAL + "\033[0;;m Fail to check balance of ledger after attempt to add invalid transactions into the ledger. Detailed exceptions are raised.\n"
+    "Autograder terminated.")
+    raise e
+
 ######################################################
+print("\n--------- Autograder Terminated -----------")
+
 if WARNING_Count == 0:
     print("\n\n\033[32mCongratulation! You passed All the tests with 0 Warning and 0 Error.\033[0;;m \n\n")
 else:
-    test_Ledger.exportFile()
-    print("\n\n\033[33mThough the implementation does NOT has FATAL ERROR, there are {} Warnings. "
+    print("\n\n\033[33mThough the implementation does NOT have FATAL ERROR, there is (are) {} Warning(s). "
           "Running through the autograder does NOT mean your implementation is correct since there are Warnings."
-          "You should debug on your implementation to solve all Warnings. File Ledger.txt is exported in the working directory to help you debug.\033[0;;m \n\n")
+          "You should debug on your implementation to solve all Warnings. File Ledger.txt is exported in the working directory to help you debug.\033[0;;m \n\n".format(WARNING_Count))
+
+if (input("Press 'y' if you want to export the Ledger in Ledger.txt file: ").lower() == "y"):
+    test_Ledger.exportFile()
--- product/Transaction/Transaction_exceptions.py
+++ product/Transaction/Transaction_exceptions.py
@@ -1,22 +1,24 @@
-class TranactionNotBalanceError(Exception):
+class BlockchainError(Exception):
+    def __init__(self, message): super().__init__(message)
+class TranactionNotBalanceError(BlockchainError):
     def __init__(self, message="The Transaction is not Balanced, check the init function in Transaction Class."):
         self.message = message
         super().__init__(self.message)
 
 
-class TransactionDoubleSpendError(Exception):
+class TransactionDoubleSpendError(BlockchainError):
     def __init__(self, message="At least one of the input in given transaction is already used, transaction is not recorded by Ledger."):
         self.message = message
         super().__init__(self.message)
 
 
-class TransactionInNotExist(Exception):
+class TransactionInNotExist(BlockchainError):
     def __init__(self, message="At least one of the input of given Transaction is not recorded in the Ledger, transaction is not recorded by Ledger."):
         self.message = message
         super().__init__(self.message)
 
 
-class TransactionSignatureError(Exception):
+class TransactionSignatureError(BlockchainError):
     def __init__(self, message="Signature in the given inTransaction fail to Pass the Signature Pass."):
         self.message = message
         super().__init__(self.message)
