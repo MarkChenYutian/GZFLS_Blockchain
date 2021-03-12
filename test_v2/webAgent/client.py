c7827187f1fdc456fb6a71efadd1eb479c1a559c
MarkChenYutian <markchenyutian@gmail.com>
2021-03-12 10:17:15 +0800

    Update WebAgent

---
+++ test_v2/webAgent/client.py
@@ -0,0 +1,8 @@
+import json
+import requests
+
+content = {'content':'h1111'}
+r = requests.post("http://127.0.0.1:5000", json=content)
+print(json.dumps(content))
+print(r.text)
+
---
+++ test_v2/webAgent/server.py
@@ -0,0 +1,13 @@
+import json
+from flask import Flask, request
+
+app = Flask("sever")
+
+@app.route('/', methods=['POST'])
+def print_json():
+    print(request.json)
+    return "0"
+
+if __name__ == '__main__':
+    app.run()
+
